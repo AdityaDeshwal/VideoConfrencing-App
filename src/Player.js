ccf7375301cf101e311715a7d85e07227853495f
AdityaDeshwal <adityadeshwal0@gmail.com>
2023-06-28 19:58:17 +0530

    Added my Project

--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,38 @@
 import React from "react";
 import "./Player.css";
 
+function Player() {
+  return (
+    <div>
+      <div id="videos">
+        <video
+          className="video-player"
+          id="user-1"
+          autoPlay
+          playsInline
+        ></video>
+        <video
+          className="video-player"
+          id="user-2"
+          autoPlay
+          playsInline
+        ></video>
+      </div>
 
-function Player(){
-    return(
-        <div id="videos">
-        <video className="video-player" id="user-1" autoPlay playsInline>
-        </video>
-        <video className="video-player" id="user-2" autoPlay playsInline>
-        </video>
+      <div id="controls">
+        <div class="control-container" id="camera-btn">
+            <img src="video-solid.png" alt="Video on-off"/>
         </div>
-    );
+        <div class="control-container" id="mic-btn">
+            <img src="microphone-solid.png" alt="Audio on-off"/>
+        </div>
+        <a href="lobby.html">
+        <div class="control-container" id="leave-btn">
+            <img src="phone-solid.png" alt="Leave Meeting"/>
+        </div>
+        </a>
+      </div>
+    </div>
+  );
 }
 export default Player;
--- src/xyz.js
+++ src/xyz.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useEffect } from "react";
 import AgoraRTM from 'agora-rtm-sdk';
-let APP_ID="7cbb24f5619b4d5f8f1ec69f6b35ee9c";
 
 let localstream;
 let remotestream;
 let peerConnection;
-let client=await AgoraRTM.createInstance(APP_ID);
-// // let channel=
+
+let APP_ID="7cbb24f5619b4d5f8f1ec69f6b35ee9c";
+let token=null;
+let uid=String(Math.floor(Math.random()*10000));
+let client;
+let channel;
+
+let queryString=window.location.search
+let urlParams=new URLSearchParams(queryString)
+let roomId=urlParams.get('room')
+
+if(!roomId){
+  window.location='lobby.html'
+}
+
+
 const servers={
   iceservers:[
     {
@@ -16,20 +29,61 @@ const servers={
   ]
 }
 
-localstream=await navigator.mediaDevices.getUserMedia({video:true, audio:false});
-let Init= async()=>{
-  useEffect(()=>{
-    document.getElementById('user-1').srcObject=localstream 
-  },[]);
-  useEffect(()=>{
-    document.getElementById('user-2').srcObject=remotestream
-  },[]);
+localstream=await navigator.mediaDevices.getUserMedia({video:true, audio:true});
+// let Init= async()=>{
+//   // useEffect(()=>{
+//   //   document.getElementById('user-1').srcObject=localstream 
+//   // },[]);
+//   useEffect(()=>{
+//     document.getElementById('user-2').srcObject=remotestream
+//   },[]);
+// }
+
+let creatingchannel=async()=>{
+  client=await AgoraRTM.createInstance(APP_ID);
+  await client.login({uid,token})
+  channel=client.createChannel(roomId);
+  await channel.join();
+  channel.on('MemberJoined',handleUserJoined)
+  channel.on('MemberLeft',handleUserLeft)
+
+  client.on('MessageFromPeer',handleMessageFromPeer)
+  document.getElementById('user-1').srcObject=localstream 
+
+  document.getElementById('camera-btn').addEventListener('click',toggleCamera)
+  document.getElementById('mic-btn').addEventListener('click',toggleMic)
 }
 
-let createoffer=async()=>{
+let handleUserLeft=(MemberId)=>{
+  document.getElementById('user-2').style.display='none'
+}
 
+let handleMessageFromPeer=async(message,MemberId)=>{
+  message=JSON.parse(message.text)
+  if(message.type==='offer'){
+    createanswer(MemberId,message.offer)
+  }
+  if(message.type==='answer'){
+    console.log("answere received")
+    addAnswer(message.answer)
+  }
+  if(message.type==='candidate'){
+    if(peerConnection){
+      peerConnection.addIceCandidate(message.candidate)
+    }
+  }
+}
+
+let handleUserJoined=async (MemberId)=>{
+  console.log('A new user is joined',MemberId)
+  createoffer(MemberId)
+}
+
+let createPeerConnection=async(MemberId)=>{
   peerConnection=new RTCPeerConnection(servers) 
   remotestream=new MediaStream()
+  document.getElementById('user-2').srcObject=remotestream
+  document.getElementById('user-2').style.display='block'
 
   localstream.getTracks().forEach((track)=>{
     peerConnection.addTrack(track,localstream)
@@ -42,24 +96,76 @@ let createoffer=async()=>{
     })
   }
 
-
   peerConnection.onicecandidate=async(event)=>{
     if(event.candidate){
-      console.log('new ICE candidate:',event.candidate)
-    }
+      client.sendMessageToPeer({text:JSON.stringify({'type':'candidate','candidate':event.candidate})},MemberId)
+      }
   }
+
+}
+
+let createoffer=async(MemberId)=>{
+  await createPeerConnection(MemberId)
 
 
   let offer=await peerConnection.createOffer()
   await peerConnection.setLocalDescription(offer)
 
-  console.log('offer:',offer)
+  //console.log('offer:',offer)
+  client.sendMessageToPeer({text:JSON.stringify({'type':'offer','offer':offer})},MemberId)
 }
-createoffer()
+
+let createanswer=async(MemberId,offer)=>{
+  await createPeerConnection(MemberId)
+
+  await peerConnection.setRemoteDescription(offer)
+
+  let answer=await peerConnection.createAnswer()
+  await peerConnection.setLocalDescription(answer)
+  client.sendMessageToPeer({text:JSON.stringify({'type':'answer','answer':answer})},MemberId)
+}
+
+let addAnswer=async(answer)=>{
+  if(!peerConnection.currentRemoteDescription){
+    peerConnection.setRemoteDescription(answer)
+  }
+}
+creatingchannel()
+
+let leavechannel=async()=>{
+  await channel.leave()
+  await client.logout()
+}
+
+let toggleCamera=async()=>{
+  let videoTrack=localstream.getTracks().find(track => track.kind==='video')
+  if(videoTrack.enabled){
+    videoTrack.enabled=false;
+    document.getElementById('camera-btn').style.backgroundColor='rgb(255,80,80)'
+  }
+  else{
+    videoTrack.enabled=true
+    document.getElementById('camera-btn').style.backgroundColor='rgb(179,102,249,.9'
+  }
+}
+
+let toggleMic=async()=>{
+  let audioTrack=localstream.getTracks().find(track => track.kind==='audio')
+  if(audioTrack.enabled){
+    audioTrack.enabled=false;
+    document.getElementById('mic-btn').style.backgroundColor='rgb(255,80,80)'
+  }
+  else{
+    audioTrack.enabled=true
+    document.getElementById('mic-btn').style.backgroundColor='rgb(179,102,249,.9'
+  }
+}
+
+window.addEventListener('beforeunload',leavechannel)
 
 
 function Runplayer(){
-  Init()
+  // Init()
   return(
     <h1>working</h1>
   );
